eea23f150a46a60adf8cd033bbdeb02079545474
Yan Oliveira <yan.brito@hotmail.com>
2019-10-18 19:38:42 -0300

    Created class App to call middleware and routes

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    // Para permitir a utilização da estrutura de dados JSON
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+// Exportando o arquivo app a classe App o método server
+export default new App().server;
