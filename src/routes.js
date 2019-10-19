459003ea06bbeac81c8724296b73f72ff9ec9849
Yan Oliveira <yan.brito@hotmail.com>
2019-10-18 21:31:22 -0300

    Update patterns of code

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,7 @@ const routes = new Router();
 
 // Criando a primeira rota
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello'});
-})
+routes.get('/', (req, res) => res.json({ message: 'Hello' }));
 
 // Exportanto o arquivo routes.js
 export default routes;
