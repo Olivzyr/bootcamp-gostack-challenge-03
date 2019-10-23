6dba350392125e275f7a27e38289c9e3d2c821e9
Yan Oliveira <yan.brito@hotmail.com>
2019-10-22 23:58:34 -0300

    routes to access backend to frontend

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,30 @@
 import Router from 'express';
+// Conexão entre frontEnd e BackEnd do body para o banco.
+import UserController from './app/controllers/UserController';
+import StudentController from './app/controllers/StudentController';
+import SessionController from './app/controllers/SessionController';
+
+// Middlewares
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 // Criando a primeira rota
+routes.post('/users', UserController.store);
 
-routes.get('/', (req, res) => res.json({ message: 'Hello' }));
+routes.post('/sessions', SessionController.store);
+
+// Middleware para autenticação de rotas
+routes.use(authMiddleware);
+
+// Rota para atualização de informações do usuário
+routes.put('/users', UserController.update);
+
+// Rota para cadastro de alunos
+routes.post('/cadastrados/atualizados', StudentController.store);
+
+// Rota para atualização de informações dos alunos
+routes.put('/cadastrados/atualizados', StudentController.update);
 
 // Exportanto o arquivo routes.js
 export default routes;
