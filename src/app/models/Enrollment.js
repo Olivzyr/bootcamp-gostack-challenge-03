a422e93f013087fe52a8e887e54bca84ae44d815
SkullDarth <yan.brito@hotmail.com>
2019-11-02 22:31:43 -0300

    Create migration enrollment and ForeignKey access

---
+++ src/app/models/Enrollment.js
@@ -0,0 +1,24 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Enrollment extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        start_date: Sequelize.DATE,
+        end_date: Sequelize.DATE
+      },
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
+
+  // Gerando o relacionamento entre tabelas
+  static associate(models) {
+    this.belongsTo(models.Student, { foreignKey: 'student_id', as: 'student' });
+    this.belongsTo(models.Plan, { foreignKey: 'plan_id', as: 'plan' });
+  }
+}
+
+export default Enrollment;
---
+++ src/database/migrations/20191102171859-create-enrollments.js
@@ -0,0 +1,66 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('enrollments', { 
+      /**
+       * Colunas principais da tabela (sem relacionamento)
+       */
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      start_date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      end_date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      
+      /**
+       * Colunas da tabela com relacionamento  (chave estrangeira)
+       */
+
+       student_id: {
+         type: Sequelize.INTERGER,
+         references: { model: 'students', key:' id' },
+         onUpdate: 'CASCADE',
+         onDelete: 'SET NULL',
+         allowNull: true
+       },
+
+       plan_id: {
+         type: Sequelize.INTEGER,
+         references: { model: 'plans', key: 'id' },
+         onUpdate: 'CASCADE',
+         onDelete: 'SET NULL',
+         allowNull: true
+       },
+
+       /**
+        * Colunas referente a data de criação e atualização das informações
+        */
+       created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+
+
+    });
+
+  },
+
+  down: (queryInterface,) => {
+    return queryInterface.dropTable('enrollments');
+  }
+};
